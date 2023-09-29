649a4ce5ee7db1c95bec37fb0591939870eca38e
Nayak <anuja.nayak@capgemini.com>
2023-09-29 23:59:55 +0530

    Complete Code

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Home.css";
+
+function Home() {
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const handleChange = (event) => {
+    setName(event.target.value);
+
+    setGender("");
+  };
+  const onBtnClick = () => {
+    const url = `https://api.genderize.io/?name=${name}`;
+    axios
+      .get(url)
+      .then((response) => {
+        setGender(response.data.gender);
+
+        console.log("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="mainContainer">
+      <div className="container">
+        <span className="subTitle-Text">GuessGen</span>
+        <span className="subTitle-Text1">Predicting Gender from Names</span>
+        <div className="inputContainer">
+          <input
+            placeholder="Enter Name..."
+            type="text"
+            onChange={handleChange}
+            value={name}
+            className="inputField"
+          ></input>
+          <button className="submitButton" onClick={onBtnClick}>
+            Submit
+          </button>
+          {
+          gender === null ? 
+            <span>Unable to detect Gender</span>
+            
+           : gender !== ""? 
+            <span className="genderText">
+              Gender was <span className="highlightText">{gender.toUpperCase()}</span>
+            </span>
+            : null
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
